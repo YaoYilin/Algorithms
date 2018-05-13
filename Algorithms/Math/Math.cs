e163cfec7f0daf819c484bc2a12e35a37e862c78
YaoYilin <elynn.yao@qq.com>
2018-05-13 16:48:20 +0800

    add some functions

--- Algorithms/Math/Math.cs
+++ Algorithms/Math/Math.cs
@@ -8,6 +8,11 @@ namespace Algorithms.Math
 {
     public class Math
     {
+        /// <summary>
+        /// calculate the square value of a positive number greater than 0
+        /// </summary>
+        /// <param name="x">input</param>
+        /// <returns>sqrt</returns>
         public static uint Sqrt(uint x)
         {
             uint m = 0x40000000, y = 0, b;
@@ -23,6 +28,40 @@ namespace Algorithms.Math
                 m >>= 2;
             }
             return y;
+        }
+
+        /// <summary>
+        /// calculate the absolute value of a number
+        /// </summary>
+        /// <param name="x">input</param>
+        /// <returns>absolute value</returns>
+        public static int Abs(int x)
+        {
+            return ((x >> 30) | 1) * x;
+        }
+
+        /// <summary>
+        /// calculate the average of two numbers
+        /// </summary>
+        /// <param name="x">input a</param>
+        /// <param name="y">input b</param>
+        /// <returns>averange</returns>
+        public static int Averange(int x, int y)
+        {
+            // floor
+            return (x & y) + ((x ^ y) >> 1);
+            // ceil
+            //return (x | y) - ((x ^ y) >> 1);
+        }
+
+        /// <summary>
+        /// gets a symbol of a number, greater than 0 returns 1, less than 0 returns-1, equals 0 returns 0
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>symbol</returns>
+        public static int Sign(int x)
+        {
+            return -(-x >> 31) | (x >> 31);
         }
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 * URL:[https://github.com/YaoYilin/Algorithms](https://github.com/YaoYilin/Algorithms)
 
 some interesting algorithms.
+---
+
+## Math
+* Math.Sqrt(uint x)
+* Math.Abs(int x)
+* Math.Averange(int x, int y)
+* Math.Sign(int x)
