f794e6206b96af39646d30af34f3fb05b63dd5e7
YaoYilin <elynn.yao@qq.com>
2018-05-13 12:19:54 +0800

    init

---
+++ Algorithms/Math/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Math
+{
+    public class Math
+    {
+        public static uint Sqrt(uint x)
+        {
+            uint m = 0x40000000, y = 0, b;
+            while(m != 0)
+            {
+                b = y | m;
+                y >>= 1;
+                if(x >= b)
+                {
+                    x = x - b;
+                    y |= m;
+                }
+                m >>= 2;
+            }
+            return y;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Math;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Math.Sqrt(16));
+            Console.WriteLine(Math.Math.Sqrt(17));
+            Console.WriteLine(Math.Math.Sqrt(1000));
+            Console.WriteLine(System.Math.Sqrt(16));
+            Console.WriteLine(System.Math.Sqrt(17));
+            Console.WriteLine(System.Math.Sqrt(1000));
+
+            Console.ReadKey();
+        }
+    }
+}
